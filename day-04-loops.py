d9af4ae0e68789237b1228a4b41421e9efdafee6
Shivjot Choudhary <shivjotchaudhary0@gmail.com>
2026-01-24 19:02:04 +0000

    Day 4: loops practice

---
+++ day-04-loops.py
@@ -0,0 +1,16 @@
+
+# Day 4 - Loops Practice
+
+print("Numbers 1 to 10:")
+for i in range(1, 11):
+    print(i)
+
+print("\nSum of 1 to 100:")
+total = sum(range(1, 101))
+print(total)
+
+print("\nFactorial of 5:")
+fact = 1
+for i in range(1, 6):
+    fact *= i
+print(fact)
