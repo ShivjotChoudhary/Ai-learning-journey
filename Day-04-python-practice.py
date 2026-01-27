8b02d95e11072842b6a6546f13a23311728e5ebb
Shivjot Choudhary <166894894+ShivjotChoudhary@users.noreply.github.com>
2026-01-27 21:53:13 +0530

    range() function

--- Day-04-python-practice.py
+++ Day-04-python-practice.py
@@ -1,1 +1,38 @@
+# **Range( ) Function:-**
 
+#27/01/2026
+#Q55)Range()Function:-
+for el in range(5):
+  print(el)
+
+#Q56)Range()Function:-
+for el in range(1,5):
+  print(el)
+
+#Q57)Range()Function:-
+for el in range(1,6,2):
+  print(el)
+
+#Q58)Range()Function:-
+for el in range(7,2):
+  print(el)
+
+#Q59 practice questions:-
+# using for & Range():-
+# print numbers from 1 to 100
+for el in range(1,101):
+  print(el)
+
+
+#Q60 practice questions:-
+# using for & Range():-
+# print numbers from 100 to 1 :-
+a = range(1,101)
+b = a[::-1]
+for el in b:
+  print(el)
+
+#Q61) print the multiplication table of a number n.
+n = int(input("Enter the number:-"))   
+for a in range(0,11): 
+  print(a*n) 
