9d9bdf0209511ec8a432cfbcde868a2252bbd35d
Shivjot Choudhary <166894894+ShivjotChoudhary@users.noreply.github.com>
2026-01-30 18:38:57 +0530

    Recursion

--- Day-07-python-practice.py
+++ Day-07-python-practice.py
@@ -6,3 +6,28 @@ def show(n):
   print(n)
   show(n-1)
 show(10)
+#Q69) factorial:-
+def fac_to(n):
+  if n==0 or n==1:
+    return 1
+  return fac_to(n-1)*n
+  print(n)
+fac_to(4)
+
+#70) practicing recurton questions :-
+#write a recursive function to calculate the sum of first n natural numbers.
+def na_tural(n):
+  if n==0:
+    return 0
+  return na_tural(n-1)+n
+na_tural(5)
+
+#Q71)write a recursive function to print all the element of the list.
+# use list & index as a parameter.
+def li_st(a,b=0):
+  if b == len(a):
+    return 
+  print(a[b])
+  return li_st(a,b+1)
+d = [8,3,4,5,"shiv","rohit"]
+li_st(d)
