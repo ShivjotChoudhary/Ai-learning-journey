1bed3d1d43e2876c075c303262db38b5c0faca4c
Shivjot Choudhary <166894894+ShivjotChoudhary@users.noreply.github.com>
2026-01-31 23:35:18 +0530

    file operations

--- Day-08-python-practice.py
+++ Day-08-python-practice.py
@@ -1,1 +1,17 @@
+#31/01/2026
+#Q76)appending a file:-
+f = open("/content/2ndexp.txt","a")
+a = f.write("\nshivyyy")
+f.close()
+
+#Q77) opening in "r+" mode:-
+f = open("/content/sample.txt","w+")
+a = f.write("shivyy")
+print(f.read())
+f.close()
+
+#Q78) create a new file "practice.txt" using python. Add the following data in it :-
+with open("practice.txt","w") as a:
+  b = a.write(" Hi everyone \n we are learning file I/O \n using java \n I like programming in java")
+
 
